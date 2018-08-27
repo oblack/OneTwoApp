679eb9b8b1fdb30c324726121698f677711f8b6b
Orlin Genchev <ogenchev@gmail.com>
2018-08-27 19:32:36 +0300

    Main app using both One and Two middle tier libs

---
+++ src/print/Print.java
@@ -0,0 +1,16 @@
+package print;
+
+import one.One;
+import two.Two;
+
+public class Print {
+    public static void main(String []args) {
+        Two two = new Two();
+        two.print();
+
+        One one = new One();
+        one.print();
+
+
+    }
+}
